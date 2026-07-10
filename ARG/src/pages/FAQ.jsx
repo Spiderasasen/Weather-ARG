6d2666d316a2021165ad201587a4cdafe82a90af
spiderasasen <spiderasasen@gmail.com>
2026-07-10 12:36:08 -0400

    made the other pages

---
+++ ARG/src/pages/FAQ.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+function FAQ() {
+    return (
+        <div>
+            <Header/>
+            <h1>FAQ</h1>
+        </div>
+    )
+}
+export default FAQ;
---
+++ ARG/src/pages/Join.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+function Join() {
+    return (
+        <div>
+            <Header />
+            <h1>Join</h1>
+        </div>
+    )
+}
+export default Join;
