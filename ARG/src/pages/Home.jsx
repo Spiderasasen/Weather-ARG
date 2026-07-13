fa8b6674da34541d96098e336b36b110d89b4006
spiderasasen <spiderasasen@gmail.com>
2026-07-13 09:13:04 -0400

    gave myself instructions on this branch

--- ARG/src/pages/Home.jsx
+++ ARG/src/pages/Home.jsx
@@ -11,3 +11,12 @@ function Home() {
     )
 }
 export default Home;
+/*TODO:
+* add a fake weather prediction for the week
+*   show the temperature for the week
+*   add a chance of rain section
+*   add predictions for the next 7 days
+*   it will check the month of the user to give the correct nanomoles for the main game
+* have a message under the main weather saying that the weather is alright
+* add a footer that will only relocate the user to the join us or about us page
+* */
