20bcb142257f868fab2265015dac4dd0501bbbae
spiderasasen <spiderasasen@gmail.com>
2026-07-13 12:02:13 -0400

    did the faq section

--- ARG/src/pages/FAQ.jsx
+++ ARG/src/pages/FAQ.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header"
 import {tab_name} from "../util/tab_name"
 import Footer from "../components/Footer"
+import FAQ_data from "../data/FAQ.json"
+import "../styles/faq.css"
 
 function FAQ() {
     tab_name("FAQ");
@@ -8,7 +10,15 @@ function FAQ() {
     return (
         <div>
             <Header/>
-            <h1>FAQ</h1>
+            <div className="faq">
+                <h1>FAQ</h1>
+                {FAQ_data.map((item) => (
+                    <div key={item.id}>
+                        <h2>{item.question}</h2>
+                        <p>{item.answer}</p>
+                    </div>
+                ))}
+            </div>
             <Footer/>
         </div>
     )
