d264a2b309b8ca981e253e2816f2e23ffa9a31b2
spiderasasen <spiderasasen@gmail.com>
2026-07-13 11:34:31 -0400

    got some basic style for everything

--- ARG/src/pages/Home.jsx
+++ ARG/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header"
 import {tab_name} from "../util/tab_name"
 import Footer from "../components/Footer"
 import Weather_Prediction from "../components/Weather_Prediction"
+import "../styles/main.css"
 
 function Home() {
     tab_name("Home");
@@ -17,11 +18,3 @@ function Home() {
     )
 }
 export default Home;
-/*TODO:
-* add a fake weather prediction for the week
-*   show the temperature for the week
-*   add a chance of rain section
-*   add predictions for the next 7 days
-*   it will check the month of the user to give the correct nanomoles for the main game
-* have a message under the main weather saying that the weather is alright
-* */
