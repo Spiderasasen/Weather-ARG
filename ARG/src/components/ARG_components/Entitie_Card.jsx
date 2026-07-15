a4ef3cea7b2abc9fbb447509c0ac45ec9e8b1ee0
spiderasasen <spiderasasen@gmail.com>
2026-07-15 11:57:23 -0400

    this did not work. going back to the orginal idea and fighting it from there

--- ARG/src/components/ARG_components/Entitie_Card.jsx
+++ ARG/src/components/ARG_components/Entitie_Card.jsx
@@ -2,37 +2,30 @@ import entities from "../../data/entities.json"
 import {useEffect, useState} from "react";
 
 function Entitie_Card() {
-    // const [entitiesLore, setEntitiesLore] = useState([]);
-    //
-    // useEffect(() => {
-    //     async function load() {
-    //         //loading the json
-    //         const json = await fetch(entities).then(res => res.json());
-    //
-    //         //loading each text file listed in the json
-    //         const loadedTexts = await Promise.all(
-    //             json.files.map(path =>
-    //             fetch(path).then(r => r.text()))
-    //         );
-    //         setEntitiesLore(loadedTexts);
-    //     }
-    //     load();
-    // }, [])
+    const [texts, setTexts] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            // Load your JSON
+            const json = await fetch(entities).then(r => r.json());
+
+            // Load each text file listed in the JSON
+            const loadedTexts = await Promise.all(
+                json.files.map(path =>
+                    fetch(path).then(r => r.text())
+                )
+            );
+
+            setTexts(loadedTexts);
+        }
+
+        load();
+    }, []);
 
     return(
         <div>
-            {entities.map((entity) => (
-                <div key={entity.id}>
-                    <h2>{entity.name}</h2>
-                    <h3>Active: {entity.active}</h3>
-                    <p>{entity.summary}</p>
-                    <p>
-                        <strong>How to Survive:</strong>
-                        <br/>
-                        <em>{entity.How_to_survive}</em>
-                    </p>
-                    <p>{entity.lore}</p>
-                </div>
+            {texts.map((t, i) => (
+                <pre key={i}>{t}</pre>
             ))}
         </div>
     );
