94c1fee2b35f19015d517f81d1c0b22460fd1297
spiderasasen <spiderasasen@gmail.com>
2026-07-15 12:24:50 -0400

    small change

--- ARG/src/pages/ARG_pages/ARG_Home.jsx
+++ ARG/src/pages/ARG_pages/ARG_Home.jsx
@@ -1,8 +1,11 @@
 import ARG_Header from "../../components/ARG_components/ARG_Header.jsx";
 import "../../styles/main.css"
 import ARG_Footer from "../../components/ARG_components/ARG_Footer.jsx";
+import {tab_name} from "../../util/tab_name.ts";
 
 function ARG_Home() {
+    tab_name("Assignment")
+
     return(
         <div  className="ARG_main">
             <ARG_Header />
--- ARG/src/pages/ARG_pages/Entities.jsx
+++ ARG/src/pages/ARG_pages/Entities.jsx
@@ -2,8 +2,11 @@ import ARG_Header from "../../components/ARG_components/ARG_Header.jsx";
 import ARG_Footer from "../../components/ARG_components/ARG_Footer.jsx";
 import Entitie_Card from "../../components/ARG_components/Entitie_Card.jsx";
 import "../../styles/main.css"
+import {tab_name} from "../../util/tab_name.ts";
 
 function Entities() {
+    tab_name("Entities")
+
     return(
         <div className="ARG_main">
             <ARG_Header />
